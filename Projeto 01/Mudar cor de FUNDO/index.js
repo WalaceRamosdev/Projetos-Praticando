d38c47939ab6b5a798926118179b177ada70fcb8
Walace Ramos <contatowalace.dev@gmail.com>
2024-05-13 11:34:58 -0300

    finalizado

--- Projeto 01/Mudar cor de FUNDO/index.js
+++ Projeto 01/Mudar cor de FUNDO/index.js
@@ -1,38 +1,33 @@
-
-
-/* De inicio, precisamos definir uma variável que vai estar guardando todas as cores que irão aparecer na página de nosso site, afinal, o javascript não irá criar uma cor do nada, precisamos deixar elas definidas para que assim possamos estar utilizando elas quando quisermos */
+/* Na nossa primeira variável, vamos estar adicionando as cores que vao ser selecionadas aleatoriamente pelo javascript */
 
 const colors = ['Red', 'Blue', 'Grey', 'Yellow', 'Green', 'Cyan' , 'Orange', 'Pink', 'Purple', 'Brown', 'Aqua', 'Aquamarine']
 
-/* Agora iremos criar uma variável para o botão que será peça fundamental para mudança das cores na nossa página */
+/* Agora, iremos criar a variável para o botão da página puxando direto de sua id no nosso HTML */
 
 const btn = document.getElementById('btn')
 
-/* Logo abaixo, teremos a variável que será responsável por enviar o nome da cor selecionada.
-Ela fará com que a cor que for gerada tenha seu nome associado e apresentado no HTML */
+/* Por fim, vamos estar adicinando a ultima variável de nosso span, aonde aparecerá o nome da cor atul da página...usaremos a classe que está atribuida a ele no HTML, portando, utilizaremos o querySelector para isso 
+*/
 
 const color = document.querySelector('.color')
 
-/* Nesta parte, iremos dar sentido para o button de nossa página, faremos com que ele aguarde um evento dentro do HTML para assim, estar fazendo o que lhe é proposto, que é trocar a cor de fundo da nossa página. Para isso, vamos estar adicionando um eventListenner a ele com a variavel de cores que listamos mais acima.
-Esse processo ocorrerá sempre que um click for feito no botão da página */
-
-btn.addEventListener('click', function(){
-    const randomColor = getRandomColor()
-    document.body.style.background = colors [randomColor]
-
-    /* Aqui iremos chamar a variável color para que o nome da cor seja associada e enviada para a parte principal da página */
-
-    color.textContent = colors[randomColor]
-})
-
-/* Aqui iremos criar uma fuction que será chamada logo acima no nosso código para estar gerando assim, uma cor aleatória.
-Para isso, iremos criar a nossa function que irá retornar a cor aleatória, para isso, iremos utilizar o método Math.random, mas iremos usá-lo dentro de outro Math, que é o Math.floor...
-O Math.random irá nos retornar números fracionados, que não é interessante para nosso uso, aqui, precisamos de números inteiros, por isso devemos usá-lo dentro de Math.floor 
-
-Logo após definirmos nossos métodos, precisamos retornar todas as cores que irão ser selecionandas aleatóriamente, para isso, logo após o Math.random, iremos multiplicá-lo pela variável colors.lenght, que dirá a quantidade exata de objetos dentro da variável colors.
-
+/* Aqui iremos criar no função responsável pela seleção aleatória das cores listas na nossa primeira variável. Utilizaremos dois métodos Math juntos, floor e random.
+O método Math.random é responsável por retornar números aleatórios para a console.log, o que não é interessante para nosso uso no momento, ja que temos itens dentro de uma array na primeira variável, e o que precisamos é de números inteiros, aí que o método Math.floor é empregado. Quando utilizamos o Math.random dentro de Math.floor, nos é retornado números inteiros, agora para termos a quantidade exata de cores dentro da nossa array, iremos multiplicar o Math.random pela variável colors.length (length = é a quantidade de objetos ou itens dentro da array, por isso, caso alguma cor seja adicionada ou removida da array, automaticamente ela é informada a função que a quantidade foi alterada)
 */
+
 
 function getRandomColor () {
     return Math.floor(Math.random() * colors.length)
 }
+
+/* Para que tudo aconteça, vamos finalmente integrar a função ao button da página, para isso, iremos adicinar um evento que vá acontecer com o button, ou seja, o javascript irá estar aguardando este evento para que possa rodar a função, neste caso, iremos adicionar o evento 'click' ao evento, logo ao lado iremos adicionar uma outra função para que as demais coisas venham rodar na página.
+Iremos criar mais uma variável, que irá recebera function que criamos logo  acima, após isso, precisamos dizer o que essa função irá fazer, que no caso será ir até o documento da página HTML, encontrar o arquivo body que está dentro de style e assim chegarmos até o background que é o que será alterado. 
+Para termos uma página mais dinâmica, iremos também enviar o nome da cor atual para ser exibida, e para isso, iremos utilizar a variável color que foi criada no inicio de do script, nela iremos adicionar o text.Content que irá receber também a variável 
+colors [randomColor]
+*/
+
+btn.addEventListener('click', function() {
+    const randomColor = getRandomColor()
+    document.body.style.background = colors [randomColor]
+    color.textContent = colors [randomColor]
+})
