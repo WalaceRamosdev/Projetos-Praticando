5ff1609892d9dd80834919eda8da305daf5338a8
Walace Ramos <contatowalace.dev@gmail.com>
2024-05-27 21:40:56 -0300

    documentação

--- Projeto 01 - Change Color/Mudar cor de FUNDO/index.js
+++ Projeto 01 - Change Color/Mudar cor de FUNDO/index.js
@@ -9,7 +9,10 @@ const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
 /* Esta function será responsavel por toda a seleção aleatória das cores que serão passadas para o backgroun.
-Iremos utilizar dois Math
+Iremos utilizar dois Math para termos o resultado desejado */
+
+/* Caso usassemos apenas o Math.random teriamos o retorno de números fracionados o que não é interessante para nós, para resvolermos isto, iremos utilizar ele dentro de outro math, o Math.floor, que irá forçar o retorno de números inteiros...logo após iremos puxar as cores selecionadas na nossa primeira const, puxadas as cores, iremos usar o 
+.length para usarmos todas as cores que estão dentro da array */
 
 const getRandomColor = () => {
     return Math.floor(Math.random() * colors.length)
