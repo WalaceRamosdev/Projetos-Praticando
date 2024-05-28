d6e84c75972370c1c751bbf799f0ef3c5e56b604
Walace Ramos <contatowalace.dev@gmail.com>
2024-05-27 21:35:08 -0300

    documentando

--- Projeto 01 - Change Color/Mudar cor de FUNDO/index.js
+++ Projeto 01 - Change Color/Mudar cor de FUNDO/index.js
@@ -1,7 +1,15 @@
+/* Para iniciarmos, devemos definir quais cores irão fazer parte do nosso projeto. Elas serão postas dentro de uma array */
+
 const colors = ['Red', 'Blue', 'Grey', 'Yellow', 'Green', 'Cyan' , 'Orange', 'Pink', 'Purple', 'Brown', 'Aqua', 'Aquamarine']
+
+/* Aqui abaixo iremos puxar os itens que iremos trabalhar dentro do nosso HTML através do JavaScript.
+Iremos puxa-los através de seus id e sua classe */
 
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
+
+/* Esta function será responsavel por toda a seleção aleatória das cores que serão passadas para o backgroun.
+Iremos utilizar dois Math
 
 const getRandomColor = () => {
     return Math.floor(Math.random() * colors.length)
