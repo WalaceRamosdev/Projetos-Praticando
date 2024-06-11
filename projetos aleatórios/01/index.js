85d893046cd205184923db2d463186e382f939ad
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-11 13:20:38 -0300

    vetores

--- projetos aleatórios/01/index.js
+++ projetos aleatórios/01/index.js
@@ -1,10 +1,9 @@
-let n1 = [], n2 = [], n3 = [], n4 = [], media = []
-for(let n = 0; n <= 49; n++)
-{
-    n1[n] = prompt()
-    n2[n] = prompt()
-    n3[n] = prompt()
-    n4[n] = prompt()
-    media[n] = (n1[n] + n2[n] + n3[n] + n4[n])/4
+let listaDeNomes = [];
 
+for (let x = 0; x < 100; x++) {
+    listaDeNomes[x] = prompt();
+}
+
+for (let x = 0; x < 100; x++) {
+    console.log(listaDeNomes[x]);
 }
