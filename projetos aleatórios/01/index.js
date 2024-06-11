4e7f937000ca2c583e6f6e2580461fc7989e5b3f
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-11 13:49:20 -0300

    matrizes

--- projetos aleatórios/01/index.js
+++ projetos aleatórios/01/index.js
@@ -1,14 +1,10 @@
-let tabela = [
-    [54, 21, 3],
-    [29, 99, 306],
-    [76, 5, 11],
+let matriz = [
+    [-1, 2, -5],
+    [3, 0, -3],
+    [5, 7, -6],
 ];
+    let n = 3;
 
-    for (let x = 0; x <= 2; x++) {
-        console.log("Repetição " + x + " do primeiro para.");
-
-    for (let y = 0; y <= 2; y++) {
-        console.log("Repetição " + y + " do segundo para.");
-        console.log("Linha " + x + " / Coluna " + y + ": " + tabela[x][y]);
-    }
+    for (let x = 0; x < n; x++) {
+        console.log(matriz[x][x]);
 }
