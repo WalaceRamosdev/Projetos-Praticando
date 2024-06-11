86fc807f6d649dd6503b9a62eb2ec5423eef1972
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-11 13:08:09 -0300

    continue

--- projetos aleatórios/01/index.js
+++ projetos aleatórios/01/index.js
@@ -1,16 +1,5 @@
-
-
-for (let x = 1; x <= 10; x++) {
-
-    for (let y = 1; y <= 5; y++) {
-        
-        console.log("X: " + x + " - Y: " + y);
-
-        if ((x + y) % 3 == 0) {
-        console.log("É MÚLTIPLO DE 3. PAROU!");
-
-        break;
-
-        }
-    }
+for (let i = 1; i <= 10; i++) {
+    console.log(`O valor de i é ${i}`);
+    continue;
+    console.log(`O próximo valor de i será ${i + 1}`);
 }
